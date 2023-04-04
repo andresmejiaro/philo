1406c83586997c7a1fdcf8aa1c5a82353138332d
Andres Mejia <amejia@c3r6s1.42madrid.com>
2023-04-04 23:53:08 +0200

    Learning about mutex

--- philo/main.c
+++ philo/main.c
@@ -6,13 +6,13 @@
 /*   By: amejia <amejia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 19:40:48 by amejia            #+#    #+#             */
-/*   Updated: 2023/04/03 23:46:32 by amejia           ###   ########.fr       */
+/*   Updated: 2023/04/04 23:50:32 by amejia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	start_params(int argc, char *argv, t_parameters *params)
+void	start_params(int argc, char **argv, t_parameters *params)
 {
 	params->n_fork = ft_atoi(argv[1]);
 	params->time_die = ft_atoi(argv[2]);
@@ -20,41 +20,72 @@ void	start_params(int argc, char *argv, t_parameters *params)
 	params->time_sleep = ft_atoi(argv[4]);
 	if (argc >= 6)
 		params->n_eat = ft_atoi(argv[5]);
-	gettimeofday(params->time_start, NULL);
+	gettimeofday(&((*params).time_start), NULL);
 }
 
-void	philo_state_start(t_philo_state *state, t_parameters *params)
+void	philo_state_start(t_philo_state *state, int id)
 {
-	gettimeofday(state->time_ate, NULL);
-	state->time_slept = NULL;
-	state->time_thought = NULL;
+	gettimeofday(&((*state).time_ate), NULL);
+	state->id = id;
 	state->state = 0;
 }
 
-void	philosophy(t_parameters *params)
+void	*philosophy(void *params)
 {
 	t_philo_state	state;
+	int				k;
+	t_parameters	*truep;
 
-	philo_state_start(&state, params);
+	truep = (t_parameters *)params;
+	philo_state_start(&state, truep->id);
 	while (1)
 	{
-		gettimeofday(state.time, NULL)
-		if (state.time > state.time_ate + params->time_die)
+		gettimeofday(&(state.time), NULL);
+		k = timediff(state.time, state.time_ate);
+		if (k > (int)truep->time_die)
 		{
-			printf("Philo %d has died", state->id);
-			exit (0);
+			printf("%d ms, philo %d has died\n", k, state.id);
+			break ;
 		}
-			
+		int w = pthread_mutex_lock(&(truep->forks[0]));
+		if (w == 0)
+		{
+			printf("took a fork\n");
+			gettimeofday(&(state.time_ate),NULL);
+			printf("Ate\n");
+			usleep(truep->time_eat);
+			pthread_mutex_unlock(&(truep->forks[0]));
+		}
 	}
+	return (NULL);
 }
 
 int	main(int argc, char **argv)
 {
-	pthread_t *philo;
-	t_fork *fork;
-	t_parameters params;
-	
+	t_parameters	params;
+	pthread_t		*thread;
+	unsigned int	ct;
+	int				rc;
+
+	if (argc < 4 || argc > 6)
+		exit(0);
 	start_params(argc, argv, &params);
+	thread = (pthread_t *)malloc(params.n_fork * sizeof(thread));
+	params.forks = malloc(params.n_fork * sizeof(pthread_mutex_t));
+	ct = 1;
+	while (ct <= params.n_fork)
+	{
+		params.id = ct;
+		int m = pthread_mutex_init(&(params.forks[ct]), NULL);
+		m++;
+		rc = pthread_create(&thread[ct - 1], NULL, philosophy, (void *)&params);
+		ct++;
+	}
+	ct = 1;
+	while (ct <= params.n_fork)
+	{
+		pthread_join(thread[ct -1], NULL);
+		ct++;
+	}
 	return (0);
 }
-
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: amejia <amejia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/03 19:42:26 by amejia            #+#    #+#             */
-/*   Updated: 2023/04/03 23:44:49 by amejia           ###   ########.fr       */
+/*   Updated: 2023/04/04 21:58:25 by amejia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,28 @@
 # include <sys/time.h>
 # include <unistd.h>
 
-typedef struct s_fork {
-	int	id;
-}	t_fork;
-
 typedef struct s_parameters {
 	unsigned int	n_fork;
 	unsigned int	time_die;
 	unsigned int	time_eat;
 	unsigned int	time_sleep;
 	unsigned int	n_eat;
-	struct timeval *time_start;
+	struct timeval	time_start;
+	pthread_mutex_t	*forks;
+	int				id;
 }	t_parameters;
 
 typedef struct s_philo_state {
-	struct timeval	*time_ate;
-	struct timeval	*time_slept;
-	struct timeval	*time_thought;
-	struct timeval	*time;	
+	struct timeval	time_ate;
+	struct timeval	time_slept;
+	struct timeval	time_thought;
+	struct timeval	time;	
 	unsigned int	id;
 	unsigned int	state;
 }	t_philo_state;
 
-
-
 int	ft_atoi(char *str);
+int	main(int argc, char **argv);
+int	timediff(struct timeval tim1, struct timeval tim2);
 
 #endif
